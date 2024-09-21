8f2bb558d9e3c08f457e473d012717665bf3fcd9
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-21 11:31:20 +0430

    Hero Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import NavbarViwe from './components/NavbarViwe.vue';
+  import Hero from './components/Hero.vue';
 </script>
 
 <template>
   <div>
     <NavbarViwe/>
+    <Hero/>
   </div>
 </template>
