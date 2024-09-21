1724f3a4ef2de01b3247b6793c2e4685514dc722
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-21 11:47:14 +0430

    Home Cards0 Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import NavbarViwe from './components/NavbarViwe.vue';
   import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
 </script>
 
 <template>
   <div>
     <NavbarViwe/>
     <Hero/>
+    <HomeCards/>
   </div>
 </template>
