8f528e450921fa0786437be5595e3153991deaaa
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-23 10:32:06 +0430

    Not Found Page Setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+     <section class="text-center flex flex-col justify-center items-center h-96">
+      <i class="pi pi-exclamation-triangle text-yellow-400 fa-4x mb-4"></i>
+      <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
+      <p class="text-xl mb-5">This page does not exist</p>
+      <RouterLink
+        to="/"
+        class="text-white bg-green-700 hover:bg-green-900 rounded-md px-3 py-2 mt-4"
+        >Go Back</RouterLink
+      >
+    </section>
+</template>
